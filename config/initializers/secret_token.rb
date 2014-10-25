c7ee29c940481ec49d38db98499fb0aaa3522a8d
simp1eton <scq1993@gmail.com>
2014-10-25 02:44:06 +0000

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codeforgood2014::Application.config.secret_token = '717e0990e4a1a3af5126ff23206fa515671c6d921ca8abe8d95cb3bd58f14ca3d64c4eac4f862dea5b64444d85bc05e7cbaeb7e59d5a9d1a3b4a87f3582c008b'
